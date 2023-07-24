2af9ec2d4cf901f3f5dff048db57132717408d5f
Mr-robot <hackingtest017@gmail.com>
2023-07-24 15:05:49 -0400

    init

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Simple helper function 
+"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns the index range from a given page and page size.
+        Args:
+            page (int):
+            page_size (int):
+        Returns:
+            return (Tuple):
+    """
+
+    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
