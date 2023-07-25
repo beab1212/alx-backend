d58d97eb05de694f9c443930814af5a454077b3a
Mr-robot <hackingtest017@gmail.com>
2023-07-25 14:38:42 -0400

    LRN

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" LRU Caching """
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    class LRUCache that inherits from BaseCaching and is a caching system:
+    """
+    def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the item value
+            for the key key.
+            =>If key or item is None, this method should not
+            do anything.
+            =>If the number of items in self.cache_data is higher that
+            BaseCaching.MAX_ITEMS:
+
+                =>you must discard the least recently used item
+                (LRU algorithm)
+                =>you must print DISCARD: with the key discarded and
+                following by a new line
+        """
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                lru_key, lru_value = self.cache_data.popitem()
+                print("DISCARD:", lru_key)
+            self.cache_data[key] = item
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Return the value in self.cache_data linked to key.
+            If key is None or if the key doesn’t exist in
+            self.cache_data, return None.
+        """
+
+        return self.cache_data.get(key, None)
