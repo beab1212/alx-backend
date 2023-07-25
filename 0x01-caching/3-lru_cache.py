11d23b11642d3a7b999d706d54fc75d7976c0b0a
Mr-robot <hackingtest017@gmail.com>
2023-07-25 14:51:00 -0400

    LRN

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -9,6 +9,7 @@ class LRUCache(BaseCaching):
     """
     def __init__(self):
         super().__init__()
+        self.used_key = []
 
     def put(self, key, item):
         """assign to the dictionary self.cache_data the item value
@@ -23,20 +24,23 @@ class LRUCache(BaseCaching):
                 =>you must print DISCARD: with the key discarded and
                 following by a new line
         """
-        if key is None or item is None:
-            return
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, lru_value = self.cache_data.popitem()
-                print("DISCARD:", lru_key)
+        if key is not None and item is not None:
             self.cache_data[key] = item
-        else:
-            self.cache_data[key] = item
+            if key not in self.used_key:
+                self.used_key.append(key)
+            else:
+                self.used_key.append(
+                    self.used_key.pop(self.used_key.index(key)))
+            if len(self.used_key) > BaseCaching.MAX_ITEMS:
+                discard = self.used_key.pop(0)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
 
     def get(self, key):
         """Return the value in self.cache_data linked to key.
             If key is None or if the key doesn’t exist in
             self.cache_data, return None.
         """
-
+        if key is not None and key in self.cache_data.keys():
+            self.used_key.append(self.used_key.pop(self.used_key.index(key)))
         return self.cache_data.get(key, None)
