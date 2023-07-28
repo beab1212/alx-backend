fe2312d445484e6ec70cea7fbb72300d2063cb3a
Mr-robot <hackingtest017@gmail.com>
2023-07-28 15:09:43 -0400

    add endpage

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python3
-"""Deletion-resilient hypermedia pagination
+"""
+Deletion-resilient hypermedia pagination
 """
 
 import csv
-import math
-from typing import Dict, List, Tuple
-
-
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Retrieves the index range from a given page and page size.
-    """
-
-    return ((page - 1) * page_size, ((page - 1) * page_size) + page_size)
+from typing import List, Dict
 
 
 class Server:
@@ -21,6 +14,7 @@ class Server:
 
     def __init__(self):
         self.__dataset = None
+        self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
         """Cached dataset
@@ -33,31 +27,29 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Retrieves a page of data.
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
         """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
-        start, end = index_range(page, page_size)
-        data = self.dataset()
-        if start > len(data):
-            return []
-        return data[start:end]
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """
+        Returns a dictionary with key-value pairs
         """
-        current = []
-        data = self.indexed_dataset()
+        focus = []
+        dataset = self.indexed_dataset()
         index = 0 if index is None else index
-        keys = sorted(data.keys())
+        keys = sorted(dataset.keys())
         assert index >= 0 and index <= keys[-1]
-        [current.append(i)
-         for i in keys if i >= index and len(current) <= page_size]
-        data = [data[v] for v in current[:-1]]
-        next_index = current[-1] if len(current) - page_size == 1 else None
-        return {'index': index,
-                'data': data,
-                'page_size': len(data),
-                'next_index': next_index
-                }
+        [focus.append(i)
+         for i in keys if i >= index and len(focus) <= page_size]
+        data = [dataset[v] for v in focus[:-1]]
+        next_index = focus[-1] if len(focus) - page_size == 1 else None
+        return {'index': index, 'data': data,
+                'page_size': len(data), 'next_index': next_index}
