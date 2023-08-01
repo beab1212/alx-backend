8d3d63465ab8606fde6b228d3d5e0cdd3aa59939
Mr-robot <hackingtest017@gmail.com>
2023-08-01 15:51:56 -0400

    task 7

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -63,7 +63,7 @@ def get_locale():
 @app.route('/')
 def index():
     """ basic route """
-    return render_template('5-index.html')
+    return render_template('6-index.html')
 
 
 if __name__ == '__main__':
