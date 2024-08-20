b37d3107ad5c79a2e551c68e06bb4a69a9f1c779
Mr-robot <hackingtest017@gmail.com>
2024-08-20 20:03:50 +0300

    add key check condition

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -15,7 +15,8 @@ class FIFOCache(BaseCaching):
         add new dict to the case_date
         """
         if key is not None and item is not None:
-            if len(self.cache_data) >= self.MAX_ITEMS:
+            if (len(self.cache_data) >= self.MAX_ITEMS and
+                    key not in self.cache_data):
                 first_key = list(self.cache_data.keys())[0]
                 print("DISCARD:", first_key)
                 self.cache_data.pop(first_key)
